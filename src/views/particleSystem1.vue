4c2d86f1eefd26e5302e9533dd007f1e5a8b607f
wujc <15755081023@163.com>
2020-09-03 20:19:26 +0800

    feat: 添加粒子系统 ===> particleSystem1/particleSystem2

--- src/views/particleSystem1.vue
+++ src/views/particleSystem1.vue
@@ -40,7 +40,7 @@ export default {
       }
       function initCamera () {
         camera = new THREE.PerspectiveCamera(50, dom.offsetWidth / dom.offsetHeight, -250, 250)
-        camera.position.set(0, 0, 1000)
+        camera.position.set(0, 0, 600)
       }
 
       const initLight = () => {
@@ -57,86 +57,45 @@ export default {
       }
 
       function initObject () {
-        const n = 400
-        const r = 12
-        const count = 10000
+        const n = 1000
+        const count = 3000
 
         geometry = new THREE.BufferGeometry()
 
         const randomNum = (num) => { return Math.random() * num - num / 2 }
-        const vertices = new Float32Array(count * 3 * 3)// 每个面三个点，每个点XYZ
-        const normals = new Float32Array(count * 3 * 3)// 每个面三个点，每个点XYZ
-        const colors = new Float32Array(count * 3 * 3)// 每个面三个点，每个点XYZ
+        const vertices = new Float32Array(count * 3)// 每个面三个点，每个点XYZ
+        const colors = new Float32Array(count * 3)// 每个面三个点，每个点XYZ
 
-        let baseP = [randomNum(n), randomNum(n), randomNum(n)]
         for (let i = 0; i < count; i++) {
-          const po = [[], [], []]
-          const color = []
+          const po = [randomNum(n), randomNum(n), randomNum(n)]
+          const color = [(po[0] / n + 0.5), (po[1] / n + 0.5), (po[2] / n + 0.5)]
 
-          let j = 0
-          while (j < 3) { //  循环生成XYZ
-            po[0][j] = baseP[j] + randomNum(r)
-            po[1][j] = baseP[j] + randomNum(r)
-            po[2][j] = baseP[j] + randomNum(r)
+          vertices[3 * i + 0] = po[0]
+          vertices[3 * i + 1] = po[1]
+          vertices[3 * i + 2] = po[2]
 
-            color[j] = baseP[j] / n + 0.5
+          console.log(color)
 
-            // // 9*i第几个面 0,3,6 面的三个点的数据开始索引
-            // vertices[9 * i + 0 + j] = pa[j]
-            // vertices[9 * i + 3 + j] = pb[j]
-            // vertices[9 * i + 6 + j] = pc[j]
-
-            j++
-          }
-          const na = new THREE.Vector3(...po[0])
-          const nb = new THREE.Vector3(...po[1])
-          const nc = new THREE.Vector3(...po[2])
-
-          const ab = new THREE.Vector3()
-          const ac = new THREE.Vector3()
-
-          ab.subVectors(na, nb)
-          ac.subVectors(na, nc)
-
-          ab.cross(ac)
-          ab.normalize()
-
-          const nor = [ab.x, ab.y, ab.z]
-
-          let k = 0
-          while (k < 9) {
-            const idx1 = parseInt(k / 3)
-            const idx2 = k % 3
-
-            vertices[9 * i + k] = po[idx1][idx2]
-            normals[9 * i + k] = nor[idx2]
-            colors[9 * i + k] = color[idx2]
-            k++
-          }
-
-          baseP = [randomNum(n), randomNum(n), randomNum(n)]
+          colors[3 * i + 0] = color[0]
+          colors[3 * i + 1] = color[1]
+          colors[3 * i + 2] = color[2]
         }
 
-        console.log(vertices)
-
         geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
-        geometry.setAttribute('normals', new THREE.BufferAttribute(normals, 3))
         geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-        // geometry.setAttribute('normals')
-        // geometry.setAttribute('uvs', verrices)
 
         window.geometry = geometry
-        material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide, vertexColors: true })
-        var axesHelper = new THREE.AxesHelper(200)
+        material = new THREE.PointsMaterial({ size: 3, vertexColors: true })
+        // var axesHelper = new THREE.AxesHelper(200)
 
-        mesh = new THREE.Mesh(geometry, material)
-        scene.add(mesh, axesHelper)
+        mesh = new THREE.Points(geometry, material)
+        scene.add(mesh /** axesHelper */)
       }
 
       function animate () {
         window.stats.begin()
         render.render(scene, camera)
-        mesh.rotation.x += 0.01
+        mesh.rotation.x += 0.005
         mesh.rotation.y += 0.01
         requestAnimationFrame(animate)
         window.stats.end()
---
+++ src/views/particleSystem2.vue
@@ -0,0 +1,147 @@
+
+<script type="text/ecmascript-6">
+/**
+ * 在边长固定的立方体内绘制固定个数的随机三角形碎片
+ * 色值选取固定顶点着色
+ */
+import * as THREE from 'three'
+
+export default {
+  render: h => h('div', { ref: 'webgl', attrs: { id: 'webgl-container' } }),
+  async mounted () {
+    await this.initStats()
+    this.init()
+  },
+  methods: {
+    async initStats () {
+      // 初始化性能工具，
+    // 在渲染前后调用begin/end方法
+      const { default: Stats } = await import('stats.js')
+      window.stats = new Stats()
+      window.stats.dom.style.position = 'absolute'
+      window.stats.dom.style.top = '10px'
+      window.stats.dom.style.left = '10px'
+      document.body.appendChild(window.stats.dom)
+      window.stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+    },
+    init () {
+      const dom = this.$refs.webgl
+      let render
+      let camera
+      let scene
+      let mesh
+      let geometry
+      let material
+
+      function initThree () {
+        render = new THREE.WebGLRenderer({ antialias: true })
+        render.setSize(dom.offsetWidth, dom.offsetHeight)
+        dom.appendChild(render.domElement)
+      }
+      function initCamera () {
+        camera = new THREE.PerspectiveCamera(12, dom.offsetWidth / dom.offsetHeight, -250, 1000)
+        camera.position.set(300, 20, 1000)
+        camera.lookAt(0, -100, -1000)
+      }
+
+      const initLight = () => {
+        let light = new THREE.DirectionalLight(0xffffff, 1)
+        light.position.set(150, 150, 150)
+        scene.add(light)
+
+        light = new THREE.AmbientLight(0xffffff)
+        scene.add(light)
+      }
+
+      function initScene () {
+        scene = new THREE.Scene()
+        scene.position.x = -250
+        scene.position.y = -150
+      }
+
+      function initObject () {
+        const L = 15000// 长X
+        const W = 15000// 宽Z
+        // const H = 200// 高Y
+
+        const stepL = 200 // X间隔
+        const stepW = 250// Y间隔
+        // const stepH = 50// Z间隔
+
+        const computedArr = (n, step) => {
+          let j = 0
+          const arr = []
+          while (j <= n) {
+            arr.push(j - n / 2)
+            j += step
+          }
+          return arr
+        }
+
+        const arrL = computedArr(L, stepL)
+        const arrW = computedArr(W, stepW)
+        // const arrH = computedArr(H, stepH)
+
+        console.log(arrL, arrW)
+
+        geometry = new THREE.BufferGeometry()
+
+        const count = arrL.length * arrW.length
+
+        const vertices = new Float32Array(count * 3)// count个点XYZ
+        const colors = new Float32Array(count * 3)// count个点XYZ
+
+        arrL.forEach((l, lIdx) => {
+          arrW.forEach((w, wIdx) => {
+            vertices[(lIdx * arrL.length + wIdx) * 3 + 0] = l
+            vertices[(lIdx * arrL.length + wIdx) * 3 + 1] = 0
+            vertices[(lIdx * arrL.length + wIdx) * 3 + 2] = w
+
+            colors[(lIdx * arrL.length + wIdx) * 3 + 0] = l / L + 0.5
+            colors[(lIdx * arrL.length + wIdx) * 3 + 1] = 0.9
+            colors[(lIdx * arrL.length + wIdx) * 3 + 2] = w / W + 0.5
+          })
+        })
+
+        geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
+        geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+        window.geometry = geometry
+        material = new THREE.PointsMaterial({ size: 30, vertexColors: true })
+
+        // var axesHelper = new THREE.AxesHelper(200)
+
+        mesh = new THREE.Points(geometry, material)
+        scene.add(mesh /** axesHelper */)
+      }
+
+      let mathI = 0
+
+      function animate () {
+        window.stats.begin()
+
+        const count = geometry.attributes.position.count
+
+        for (let i = 1; i < count * 3; i += 3) {
+          geometry.attributes.position.array[i] = 45 * Math.sin(i / 3 + mathI)
+        }
+        geometry.attributes.position.needsUpdate = true
+
+        mathI += 0.03
+        render.render(scene, camera)
+        mesh.rotation.y = 0.25 * Math.PI
+        requestAnimationFrame(animate)
+        window.stats.end()
+      }
+
+      initThree()
+      initCamera()
+      initScene()
+      initLight()
+      initObject()
+      animate.call(this)
+    }
+
+  }
+}
+</script>
